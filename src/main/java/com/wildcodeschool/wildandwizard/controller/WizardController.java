270af32197e2b2228ccb784272a4071f7b58345e
Arsia <cam.lucas89@gmail.com>
2022-01-03 12:39:09 +0100

    Add course (bonus : delete course)

--- src/main/java/com/wildcodeschool/wildandwizard/controller/WizardController.java
+++ src/main/java/com/wildcodeschool/wildandwizard/controller/WizardController.java
@@ -17,91 +17,114 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
 @Controller
 public class WizardController {
 
-    @Autowired
-    private CourseRepository courseRepository;
+	@Autowired
+	private CourseRepository courseRepository;
 
-    @Autowired
-    private WizardRepository wizardRepository;
+	@Autowired
+	private WizardRepository wizardRepository;
 
-    @GetMapping("/")
-    public String getWizards(Model out) {
+	@GetMapping("/")
+	public String getWizards(Model out) {
 
-        out.addAttribute("wizards", wizardRepository.findAll());
+		out.addAttribute("wizards", wizardRepository.findAll());
 
-        return "wizards";
-    }
+		return "wizards";
+	}
 
-    @GetMapping("/wizard/register")
-    public String getRegister(Model out,
-                              @RequestParam Long idWizard) {
+	@GetMapping("/wizard/register")
+	public String getRegister(Model out, @RequestParam Long idWizard) {
 
-        Optional<Wizard> optionalWizard = wizardRepository.findById(idWizard);
-        Wizard wizard = new Wizard();
-        if (optionalWizard.isPresent()) {
-            wizard = optionalWizard.get();
-        }
-        out.addAttribute("wizard", wizard);
-        out.addAttribute("allCourses", courseRepository.findAll());
+		Optional<Wizard> optionalWizard = wizardRepository.findById(idWizard);
+		Wizard wizard = new Wizard();
+		if (optionalWizard.isPresent()) {
+			wizard = optionalWizard.get();
+		}
+		out.addAttribute("wizard", wizard);
+		out.addAttribute("allCourses", courseRepository.findAll());
 
-        // call the method getCourses in Wizard
-        List<Course> courses = new ArrayList<>();
-        Method method = getMethod(wizard, "getCourses",
-                new Class[]{});
-        if (method != null) {
-            try {
-                courses = (List<Course>) method.invoke(wizard);
-            } catch (IllegalAccessException | InvocationTargetException e) {
-                e.printStackTrace();
-            }
-        }
-        out.addAttribute("wizardCourses", courses);
+		// call the method getCourses in Wizard
+		List<Course> courses = new ArrayList<>();
+		Method method = getMethod(wizard, "getCourses", new Class[] {});
+		if (method != null) {
+			try {
+				courses = (List<Course>) method.invoke(wizard);
+			} catch (IllegalAccessException | InvocationTargetException e) {
+				e.printStackTrace();
+			}
+		}
+		out.addAttribute("wizardCourses", courses);
 
-        return "register";
-    }
+		return "register";
+	}
 
-    @PostMapping("/wizard/register")
-    public String postRegister(@RequestParam Long idWizard,
-                               @RequestParam Long idCourse) {
+	@PostMapping("/wizard/register")
+	public String postRegister(@RequestParam Long idWizard, @RequestParam Long idCourse) {
 
-        Optional<Wizard> optionalWizard = wizardRepository.findById(idWizard);
-        if (optionalWizard.isPresent()) {
-            Wizard wizard = optionalWizard.get();
+		Optional<Wizard> optionalWizard = wizardRepository.findById(idWizard);
+		if (optionalWizard.isPresent()) {
+			Wizard wizard = optionalWizard.get();
 
-            Optional<Course> optionalCourse = courseRepository.findById(idCourse);
-            if (optionalCourse.isPresent()) {
-                Course course = optionalCourse.get();
+			Optional<Course> optionalCourse = courseRepository.findById(idCourse);
+			if (optionalCourse.isPresent()) {
+				Course course = optionalCourse.get();
 
-                // call the method getCourses in Wizard
-                List<Course> courses;
-                Method method = getMethod(wizard, "getCourses",
-                        new Class[]{});
-                if (method != null) {
-                    try {
-                        courses = (List<Course>) method.invoke(wizard);
-                        courses.add(course);
-                    } catch (IllegalAccessException | InvocationTargetException e) {
-                        e.printStackTrace();
-                    }
-                }
+				// call the method getCourses in Wizard
+				List<Course> courses;
+				Method method = getMethod(wizard, "getCourses", new Class[] {});
+				if (method != null) {
+					try {
+						courses = (List<Course>) method.invoke(wizard);
+						courses.add(course);
+					} catch (IllegalAccessException | InvocationTargetException e) {
+						e.printStackTrace();
+					}
+				}
 
-                wizardRepository.save(wizard);
-            }
-        }
+				wizardRepository.save(wizard);
+			}
+		}
 
-        return "redirect:/wizard/register?idWizard=" + idWizard;
-    }
+		return "redirect:/wizard/register?idWizard=" + idWizard;
+	}
 
-    public Method getMethod(Object obj, String methodName, Class[] args) {
-        Method method;
-        try {
-            method = obj.getClass().getDeclaredMethod(methodName, args);
-            return method;
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
+	@GetMapping("/course/delete")
+	public String deleteCourse(@RequestParam Long idCourse, @Valid @NotNull @RequestParam Long idWizard) {
+		Wizard wizard;
+		try {
+			wizard = wizardRepository.findById(idWizard).orElseThrow(() -> new Exception("Wizard not found"));
+			for (Course course : wizard.getCourses()) {
+				if (course.getId() == idCourse) {
+					wizard.getCourses().remove(course);
+
+					break;
+				}
+			}
+
+			wizardRepository.save(wizard);
+
+			return "redirect:/wizard/register?idWizard=" + idWizard;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			return e.getMessage();
+		}
+
+	}
+
+	public Method getMethod(Object obj, String methodName, Class[] args) {
+		Method method;
+		try {
+			method = obj.getClass().getDeclaredMethod(methodName, args);
+			return method;
+		} catch (NoSuchMethodException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 }
